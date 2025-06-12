da95eedc251c305ebd06b905bf895ca158ce09ca
siddhantdhomse45 <siddh654@gmail.com>
2025-06-12 14:59:34 +0530

    change in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -108,11 +108,11 @@ const Footer = () => {
               <div className="mt-6">
                 <div className="flex items-center gap-3">
                   <FaLocationArrow />
-                  <p>Noida, Uttar Pradesh</p>
+                  <p>Pimple Saudagar, Pune</p>
                 </div>
                 <div className="flex items-center gap-3 mt-3">
                   <FaMobileAlt />
-                  <p>+91 123456789</p>
+                  <p>+91 1234567890</p>
                 </div>
               </div>
             </div>
